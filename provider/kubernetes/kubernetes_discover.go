f29ee9b8d63ea565451fcdc8377456a2de44900f
Robert Buck <rbuck@nuodb.com>
2018-01-19 12:11:56 -0500

    Format

--- provider/kubernetes/kubernetes_discover.go
+++ provider/kubernetes/kubernetes_discover.go
@@ -1,19 +1,20 @@
 package kubernetes
 
 import (
-    "fmt"
-    "io/ioutil"
-    meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-    "k8s.io/apimachinery/pkg/labels"
-    "k8s.io/client-go/kubernetes"
-    "k8s.io/client-go/rest"
-    "log"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 type Provider struct{}
 
 func (p *Provider) Help() string {
-    return `Kubernetes:
+	return `Kubernetes:
 
     provider:     "kubernetes"
     namespace:    The Kubernetes namespace to filter on
@@ -23,44 +24,44 @@ func (p *Provider) Help() string {
 }
 
 func (p *Provider) Addrs(args map[string]string, l *log.Logger) ([]string, error) {
-    if args["provider"] != "kubernetes" {
-        return nil, fmt.Errorf("discover-kubernetes: invalid provider " + args["provider"])
-    }
+	if args["provider"] != "kubernetes" {
+		return nil, fmt.Errorf("discover-kubernetes: invalid provider " + args["provider"])
+	}
 
-    if l == nil {
-        l = log.New(ioutil.Discard, "", 0)
-    }
+	if l == nil {
+		l = log.New(ioutil.Discard, "", 0)
+	}
 
-    namespace := args["namespace"]
-    labelKey := args["label_key"]
-    labelValue := args["label_value"]
+	namespace := args["namespace"]
+	labelKey := args["label_key"]
+	labelValue := args["label_value"]
 
-    if namespace == "" {
+	if namespace == "" {
 		l.Printf("[DEBUG] discover-kubernetes: Namespace type not provided. Using 'default'")
 		namespace = "default"
 	}
 
-    l.Printf("[DEBUG] discover-kubernetes: Using namespace=%s label_key=%s label_value=%s", namespace, labelKey, labelValue)
+	l.Printf("[DEBUG] discover-kubernetes: Using namespace=%s label_key=%s label_value=%s", namespace, labelKey, labelValue)
 
-    config, err := rest.InClusterConfig()
-    if err != nil {
-        return nil, fmt.Errorf("discover-kubernetes: %s", err)
-    }
-    client, err := kubernetes.NewForConfig(config)
-    if err != nil {
-        return nil, fmt.Errorf("discover-kubernetes: %s", err)
-    }
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, fmt.Errorf("discover-kubernetes: %s", err)
+	}
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("discover-kubernetes: %s", err)
+	}
 
-    l.Printf("[INFO] discover-aws: Select instances with %s=%s", labelKey, labelValue)
-    var podIpAddresses []string
-    labelsSet := labels.Set(map[string]string{labelKey: labelValue})
-    pods, err := client.Core().Pods(namespace).List(meta_v1.ListOptions{LabelSelector: labelsSet.AsSelector().String()})
-    if err != nil {
-        return nil, fmt.Errorf("discover-kubernetes: listing pods with label %s=%s in %s: %v", labelKey, labelValue, namespace, err)
-    }
-    for _, v := range pods.Items {
-        podIpAddresses = append(podIpAddresses, v.Status.PodIP)
-    }
+	l.Printf("[INFO] discover-aws: Select instances with %s=%s", labelKey, labelValue)
+	var podIpAddresses []string
+	labelsSet := labels.Set(map[string]string{labelKey: labelValue})
+	pods, err := client.Core().Pods(namespace).List(meta_v1.ListOptions{LabelSelector: labelsSet.AsSelector().String()})
+	if err != nil {
+		return nil, fmt.Errorf("discover-kubernetes: listing pods with label %s=%s in %s: %v", labelKey, labelValue, namespace, err)
+	}
+	for _, v := range pods.Items {
+		podIpAddresses = append(podIpAddresses, v.Status.PodIP)
+	}
 
-    return podIpAddresses, nil
+	return podIpAddresses, nil
 }
--- provider/kubernetes/kubernetes_discover_test.go
+++ provider/kubernetes/kubernetes_discover_test.go
@@ -1,32 +1,32 @@
 package kubernetes_test
 
 import (
-    "log"
-    "os"
-    "testing"
+	"log"
+	"os"
+	"testing"
 
-    "continuul.io/go-discover/provider/kubernetes"
-    discover "github.com/hashicorp/go-discover"
+	"continuul.io/go-discover/provider/kubernetes"
+	discover "github.com/hashicorp/go-discover"
 )
 
 func TestAddrs(t *testing.T) {
-    args := discover.Config{
-        "provider":          "kubernetes",
-        "namespace":         os.Getenv("AWS_REGION"),
-        "service":           "consul-role",
-    }
+	args := discover.Config{
+		"provider":  "kubernetes",
+		"namespace": os.Getenv("AWS_REGION"),
+		"service":   "consul-role",
+	}
 
-    if args["namespace"] == "" || args["service"] == "" {
-        t.Skip("Kubernetes namespace or service missing")
-    }
+	if args["namespace"] == "" || args["service"] == "" {
+		t.Skip("Kubernetes namespace or service missing")
+	}
 
-    p := &kubernetes.Provider{}
-    l := log.New(os.Stderr, "", log.LstdFlags)
-    addrs, err := p.Addrs(args, l)
-    if err != nil {
-        t.Fatal(err)
-    }
-    if len(addrs) != 3 {
-        t.Fatalf("bad: %v", addrs)
-    }
+	p := &kubernetes.Provider{}
+	l := log.New(os.Stderr, "", log.LstdFlags)
+	addrs, err := p.Addrs(args, l)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(addrs) != 3 {
+		t.Fatalf("bad: %v", addrs)
+	}
 }
